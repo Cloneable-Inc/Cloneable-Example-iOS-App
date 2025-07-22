ecf9b5083eebe26dc337238f8578b69cc55f7bf8
Tyler Collins <tyler@tylerjamescollins.com>
2025-07-22 10:11:29 -0400

    V1.0.2

--- cloneable-example-app/cloneable_example_app.swift
+++ cloneable-example-app/cloneable_example_app.swift
@@ -11,10 +11,9 @@ import CloneablePlatformiOS
 
 @main
 struct cloneable_example_app: App {
-    let endPoint: APIEndpoint = .dev
-
     // instantiate the CloneablePlatform as a StateObject to use throughout our app
-    @StateObject private var cloneable = CloneablePlatform(authType: .api, backend_env: .dev)
+    
+    @StateObject private var cloneable = CloneablePlatform(authType: .api, apiKey: YOUR_API_KEY)
     
     
     init() {
@@ -30,9 +29,6 @@ struct cloneable_example_app: App {
             ContentView()
             // add the CloneablePlatform as an environment object so that we can access it throughout the app
                 .environmentObject(cloneable)
-                .onAppear {
-                    cloneable.login(apiKey: API_KEY_HERE)
-                }
         }
     }
 }
